97379ba7309d14f8d863b85068c1ff427fee6728
Dhanishta-Dharshani <tpmsomrolc.dhanishtam7d@gmail.com>
2025-05-20 19:29:17 +0530

    stream_testing.py

--- stream_testing.py
+++ stream_testing.py
@@ -1,19 +1,11 @@
 import streamlit as st
 st.title("Planz - The Perfect Planner")
 
-# multi select box
-# first argument takes the box title second argument takes the options to show
-hobbies = st.multiselect("Productive activity: ",
+hobbies = st.multiselect("Productive activities: ",
                          ['Studies', 'Exrecise', 'Errands'])
-
-# write the selected options
 st.write("You have selected", len(hobbies), 'activities')
 
-# save the input text in the variable 'name'
-# first argument shows the title of the text input box
-# second argument displays a default text inside the text input area
-name = st.text_input("Enter the number of hours you would like to study for: ", "")
+name = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
 
-# Create a button, that when clicked, shows a text
 if(st.button("Submit")):
     st.text("Thank you!")
