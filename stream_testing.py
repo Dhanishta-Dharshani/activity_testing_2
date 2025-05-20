0f2026421fce0e1b5f5bc631ff2346d0b4d72a7a
Dhanishta-Dharshani <tpmsomrolc.dhanishtam7d@gmail.com>
2025-05-20 19:09:22 +0530

    stream_testing.py

--- stream_testing.py
+++ stream_testing.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("Hello World")
+# Selection box
+
+# first argument takes the titleof the selectionbox
+# second argument takes options
+hobby = st.selectbox("Productive Time: ",
+                     ['Studies', 'Exercise', 'Errands'])
+
+# print the selected hobby
+st.write("The productive activity that you have chosen is: ", hobby)
