da89900aabbccfaa80875f3e8cdd38ff301e8f7f
Dhanishta-Dharshani <tpmsomrolc.dhanishtam7d@gmail.com>
2025-05-20 20:44:17 +0530

    stream_testing.py

--- stream_testing.py
+++ stream_testing.py
@@ -5,17 +5,17 @@ productive = st.multiselect("Productive activities: ",
                          ['Studies', 'Exrecise', 'Errands'])
 st.write("You have selected", len(productive), 'activities')
 
-name1 = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
+pro_name = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
 
 
 self = st.multiselect("Self-care activities: ",
                          ['Skin care', 'Hair care', 'Journalling'])
 st.write("You have selected", len(self), 'activities')
 
-name2 = st.text_input("Enter the number of hours you would like to do the self-care activity for: ", "")
+self_name = st.text_input("Enter the number of hours you would like to do the self-care activity for: ", "")
 
 relax = st.multiselect("Relaxation activities: ",
                          ['Hobbies', 'Gadgets'])
 st.write("You have selected", len(relax), 'activities')
 
-name3 = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
+relax_name = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
