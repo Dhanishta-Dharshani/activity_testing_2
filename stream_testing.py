cfd8d08e66f936dcc24cbe7daa05b4745b1f4e29
Dhanishta-Dharshani <tpmsomrolc.dhanishtam7d@gmail.com>
2025-05-20 20:09:22 +0530

    stream_testing.py

--- stream_testing.py
+++ stream_testing.py
@@ -1,11 +1,24 @@
 import streamlit as st
 st.title("Planz - The Perfect Planner")
 
-hobbies = st.multiselect("Productive activities: ",
+productive = st.multiselect("Productive activities: ",
                          ['Studies', 'Exrecise', 'Errands'])
-st.write("You have selected", len(hobbies), 'activities')
+st.write("You have selected", len(productive), 'activities')
 
-name = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
+name1 = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
+
+
+self = st.multiselect("Self-care activities: ",
+                         ['Skin care', 'Hair care', 'Journalling'])
+st.write("You have selected", len(self), 'activities')
+
+name2 = st.text_input("Enter the number of hours you would like to do the self-care activity for: ", "")
+
+relax = st.multiselect("Relaxation activities: ",
+                         ['Hobbies', 'Gadgets'])
+st.write("You have selected", len(relax), 'activities')
+
+name3 = st.text_input("Enter the number of hours you would like to do the productive activity for: ", "")
 
 if(st.button("Submit")):
     st.text("Thank you!")
