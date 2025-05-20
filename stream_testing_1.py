37ba527e81d00830cf8ca148fcae90abdf8b282e
Dhanishta-Dharshani <tpmsomrolc.dhanishtam7d@gmail.com>
2025-05-20 19:47:46 +0530

    stream_testing_1.py

--- stream_testing_1.py
+++ stream_testing_1.py
@@ -3,20 +3,6 @@ stm.set_page_config(page_title="This is a Simple Streamlit WebApp")
 stm.title("This is the Home Page Geeks.")
 stm.text("Geeks Home Page")
 
-import streamlit
-from streamlit import *
-from streamlit import streamlit_card
-from streamlit_card import card
-stm.set_page_config(page_title="This is a Simple Streamlit WebApp")
-stm.title("This is the Home Page Geeks.")
-stm.text("Geeks Home Page")
-card(
-    title="Hello Geeks!",
-    text="Click this card to redirect to GeeksforGeeks",
-    image="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png",
-    url="https://www.geeksforgeeks.org/",
-)
-
 
 from streamlit_extras.stodo import to_do
 stm.set_page_config(page_title="This is a Simple Streamlit WebApp")
